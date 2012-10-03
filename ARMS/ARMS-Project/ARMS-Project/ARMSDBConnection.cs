09c8dafdf663f91b22b962a2f6d900213d5a259d
Fior Tejada <tuc33071@temple.edu>
2012-10-02 21:26:53 -0400

    added a function to the DB class
    It might need some changes later on

--- ARMS/ARMS-Project/ARMS-Project/ARMSDBConnection.cs
+++ ARMS/ARMS-Project/ARMS-Project/ARMSDBConnection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace ARMS_Project
 {
@@ -25,6 +26,45 @@ namespace ARMS_Project
             {
                 Console.WriteLine("Unable to connect to the specified database.  Please check the parameters and try again.\n"+e.ToString());
             }
+        }
+
+        
+        //This function verifies the credential of a user by accessing the DB user table
+        //It takes as parameters the username and password and it returns true if the user 
+        //is found
+        public bool verifyUserCredentials(string username, string password)
+        {
+            bool authorizedUser = false;
+
+            try
+            {
+                SqlCommand objCommand = new SqlCommand();
+                SqlDataReader dataReader = objCommand.ExecuteReader();
+
+                //calling a store procedure that returns the user and password
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "FindUser";
+
+                objCommand.Parameters.AddWithValue("@theUsername", username);
+                objCommand.Parameters.AddWithValue("@thePassword", password);
+
+                //if match is found return true, else return false
+                if (dataReader.Read())
+                {
+                    authorizedUser = true;
+                }
+                else
+                {
+                    authorizedUser = false;
+                }
+                dataReader.Close();
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return authorizedUser;
         }
     }
 }
