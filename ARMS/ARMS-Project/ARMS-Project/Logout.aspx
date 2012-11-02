36ea4a1f30bea96205eee7cf3bdaab05c4149c6b
Jessica <clark.jcar@gmail.com>
2012-11-02 12:41:03 -0400

    made logout page and functionality add to master

---
+++ ARMS/ARMS-Project/ARMS-Project/Logout.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/assets/Layout.Master" AutoEventWireup="True" CodeBehind="Logoutin.aspx.cs" Inherits="ARMS_Project.Logout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <div class="story" id="logout">
+    <h1>You have successfully logged out.</h1><br /><br />
+    <a href="Login.aspx">Click here to Login</a>
+        
+    </div>
+</asp:Content>
--- ARMS/ARMS-Project/ARMS-Project/assets/Layout.Master.cs
+++ ARMS/ARMS-Project/ARMS-Project/assets/Layout.Master.cs
@@ -11,7 +11,30 @@ namespace ARMS_Project.assets
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+               //Check if User is logged in
+                if (string.IsNullOrEmpty(Session["UserID"] as string))
+                {
+                    btnLogout.Visible = false;
+                }
+                else
+                {
+                    btnLogout.Visible = true;
+                }
+            }
+        }
 
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            //Check if User is logged in
+            if (string.IsNullOrEmpty(Session["UserID"] as string))
+            {
+                //if empty, drop session
+                Session.Clear();
+                Session.Abandon();
+                Response.Redirect("Logout.aspx");
+            }
         }
     }
 }
