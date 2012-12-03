2100ae1d51b01f0bf570b04ac1fd6222910ef66b
Nik Korablin <nikorablin@gmail.com>
2012-12-03 11:03:04 -0500

    Revert "Revert "Fixed login stuff""
    This reverts commit a45f992d498a9860e86847dbcaa9ae5f2db3cbb6.

--- ARMS/ARMS-Project/ARMS-Project/assets/Layout.Master.designer.cs
+++ ARMS/ARMS-Project/ARMS-Project/assets/Layout.Master.designer.cs
@@ -40,15 +40,6 @@ namespace ARMS_Project.assets {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// btnLogout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogout;
-        
-        /// <summary>
         /// body control.
         /// </summary>
         /// <remarks>
